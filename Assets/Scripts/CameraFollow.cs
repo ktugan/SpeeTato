bd1bd2c64a5f131ae1ae3ec4fb39901ebebb2696
Kadir Tugan <ktugan@users.noreply.github.com>
2015-05-30 17:52:05 +0200

    added moving camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject Camera;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var vec = new Vector3(transform.position.x, transform.position.y, Camera.transform.position.z);
+	    Camera.transform.position = vec;
+	}
+}
