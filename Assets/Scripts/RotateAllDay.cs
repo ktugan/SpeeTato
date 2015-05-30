4cbc569aa46cd067708ab89733fac7f05e755564
Kadir Tugan <ktugan@users.noreply.github.com>
2015-05-30 19:06:38 +0200

    RotationScript

---
+++ Assets/Scripts/RotateAllDay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class RotateAllDay : MonoBehaviour
+    {
+
+        public float RotationsPerMinute = 30f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(0, 6f * RotationsPerMinute * Time.deltaTime, 0);
+        }
+    }
+}
