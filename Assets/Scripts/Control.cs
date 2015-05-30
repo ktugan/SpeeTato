7e7af5174c6ee762f8177755fbcf6b8a323cb7e4
Kadir Tugan <kadir.tugan@gmail.com>
2015-05-31 00:28:49 +0200

    double jump!

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -5,9 +5,13 @@ public class Control : MonoBehaviour
 {
 
     public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
+    public float jumpSpeed = 10.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+
+    public int maxJumps = 2;
+
+    private int currentJump = 0;
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
@@ -16,10 +20,16 @@ public class Control : MonoBehaviour
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed;
-
+            currentJump = 0;
         }
+
+        if (currentJump < maxJumps && Input.GetButtonUp("Jump"))
+        {
+            currentJump++;
+            moveDirection.y = jumpSpeed * (1f / currentJump);
+        }
+
+
         moveDirection.y -= gravity * Time.deltaTime;
         moveDirection.z = 0;
         controller.Move(moveDirection * Time.deltaTime);
