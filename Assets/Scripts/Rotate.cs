56d3c569cdbf37e624320b84b9478a18a69e902f
Kadir Tugan <ktugan@users.noreply.github.com>
2015-05-31 13:41:58 +0200

    Pfanne

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Rotate : MonoBehaviour
+    {
+
+        public float RotationsPerMinute = 30f;
+
+        void Update()
+        {
+            transform.Rotate(0, 0, 6f * RotationsPerMinute * Time.deltaTime);
+        }
+    }
+}
