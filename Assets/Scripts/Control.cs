bc65a9c8dfc28366a33731a01280498cb4315506
Kadir Tugan <kadir.tugan@gmail.com>
2015-05-31 02:01:06 +0200

    animations aaaargh

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -9,9 +9,18 @@ public class Control : MonoBehaviour
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
 
+    private Animator anim;
+
     public int maxJumps = 2;
 
     private int currentJump = 0;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
@@ -29,9 +38,11 @@ public class Control : MonoBehaviour
             moveDirection.y = jumpSpeed * (1f / currentJump);
         }
 
-
         moveDirection.y -= gravity * Time.deltaTime;
         moveDirection.z = 0;
         controller.Move(moveDirection * Time.deltaTime);
+
+        float vel = Mathf.Abs(controller.velocity.x) + Mathf.Abs(controller.velocity.y);
+        anim.SetFloat("speed", vel);
     }
 }
