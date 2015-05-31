7a0d9598360214c54f257a7119269e454a06263c
Kadir Tugan <ktugan@users.noreply.github.com>
2015-05-31 12:00:23 +0200

    pivoted game

---
+++ Assets/Scripts/MoveObstacle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObstacle : MonoBehaviour {
+
+    public Vector2 velocity = new Vector2(-4, 0);
+    private Rigidbody2D rg;
+    void Start ()
+    {
+        rg = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    rg.velocity = velocity;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rg;
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+    }
+
+    public Vector2 jumpForce = new Vector2(0, 300);
+    void Update()
+    {
+        if (Input.GetKeyUp("space"))
+        {
+            rg.velocity = Vector2.zero;
+            rg.AddForce(jumpForce);
+        }
+    }
+
+    // Die by collision
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Die();
+    }
+
+    void Die()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
