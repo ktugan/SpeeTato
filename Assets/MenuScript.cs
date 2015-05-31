57f6260d0f15c8dd03df970bb7703c899d399673
knanw <knanw@outlook.com>
2015-05-31 13:12:00 +0200

    menü

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnClickPlay()
+    {
+        Application.LoadLevel("SpeedTato");
+    }
+}
